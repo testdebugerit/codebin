0954eb56afde75ab8a11eff9fb9fe1e04f2f2573
Anushka Yadav <130516212+testdebugerit@users.noreply.github.com>
2024-08-01 07:47:39 +0530

    Add files via upload

---
+++ firebaseConfig.ts
@@ -0,0 +1,8 @@
+ export const firebaseConfig = {
+  apiKey: "AIzaSyD5PPjqCaCeGblx4laV0Jq6uaJ3HgAbNv0",
+  authDomain: "codebin-ng-yt-dc5ed.firebaseapp.com",
+  projectId: "codebin-ng-yt-dc5ed",
+  storageBucket: "codebin-ng-yt-dc5ed.appspot.com",
+  messagingSenderId: "601037361035",
+  appId: "1:601037361035:web:7af11ea290d9d048ad698e"
+};
